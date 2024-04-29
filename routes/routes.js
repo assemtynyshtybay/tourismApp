67c9c579adbb6d88d6b711f98074ba4dd9e5fa16
assemtynyshtybay <47156211+assemtynyshtybay@users.noreply.github.com>
2024-04-29 18:24:47 +0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,12 @@ import toursRoutes from "./tour.routes.js";
 import cardRoutes from "./card.routes.js";
 
 export default function (app) {
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173/"); // Allow requests from any origin
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specified methods
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specified headers
+    next();
+  });
   app.use(express.json());
   app.use("/auth", authRoutes);
   app.use("/user", userRoutes);
