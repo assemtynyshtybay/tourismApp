5a27462d87ce966707e1e08a16e39d8dd59ab48b
assemtynyshtybay <47156211+assemtynyshtybay@users.noreply.github.com>
2024-04-30 11:53:23 +0500

    fix cors

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ import cardRoutes from "./card.routes.js";
 
 export default function (app) {
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Allow requests from any origin
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specified methods
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specified headers
     next();
